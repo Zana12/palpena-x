59a79518bd69f865a0af22f37280797163a5159a
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-18 16:47:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
+const moment = require('moment');
 
 const token = process.env.DISCORD_TOKEN;
 const prefix = config.prefix;
