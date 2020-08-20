84626832bb49ba0c01e51371c31323845862ec34
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-20 15:46:19 +0200

    updated

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Test Purposes',
 	execute(message, args) {
-		message.channel.send("Here is new Heroku!")
+		message.channel.send("Here is new Heroku and also new GitRepo!")
 	},
 };
