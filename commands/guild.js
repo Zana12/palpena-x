c69ff2c3bb60a894a57c755d1d4fbbb0aadda4db
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-21 14:02:47 +0200

    update

--- commands/guild.js
+++
@@ -1,8 +0,0 @@
-const DB = require("mongoose");
-
-const guildSchema = DB.Schema({
-    _id: DB.Schema.Types.ObjectId,
-    guildID: String,
-    guildName: String
-});
-module.exports = DB.model('Guild', guildSchema, 'guilds');
