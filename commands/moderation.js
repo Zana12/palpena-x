ee89e8933d6cb3bc5e3bcde00d8193d71984f4ed
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-21 16:25:17 +0200

    update

--- commands/moderation.js
+++ commands/moderation.js
@@ -1,33 +1,12 @@
 const Discord = require('discord.js');
+const config = require('../config.json');
 module.exports = {
 	name: 'ban',
 	description: 'Ban users from your server',
 	async execute(message, args) {
     let banned = message.mentions.users.first() || message.users.resolve(args[0]);
     let reason = args.slice(1).join(" ");
-  
-    // MESSAGES
-  
-    if (!banned) {
-      let baninfoembed = new Discord.MessageEmbed()
-        .setTitle("Command: b!ban")
-        .setDescription(
-          `**Description:** Ban a member, optional time limit. \n` +
-            "**Sub Commands:**\n" +
-            "-ban save | Ban a user and save their messages in chat. \n" +
-            "**Usage:**\n" +
-            "-ban [user] (limit) (reason) \n" +
-            "-ban save [user] (limit) (reason) \n" +
-            "**Examples:** \n" +
-            "-ban <@597253939469221891> 48h spam \n" +
-            "-ban save <@597253939469221891> 48h spam "
-        )
-        .setColor("#2C2F33");
-      message.channel.send(baninfoembed);
-  
-      return;
-    }
-  
+    if (!args.length) return message.channel.send(`:warning: **Usage:** \`${config.prefix}ban <mention> <reason>\``);
     if (message.author === banned) {
       let sanctionyourselfembed = new Discord.MessageEmbed()
         .setDescription(`Why sanction yourself? why?? lemme know?!`)
@@ -49,7 +28,7 @@ module.exports = {
     if (!message.member.permissions.has("ADMINISTRATOR")) {
       let nopermsembed = new Discord.MessageEmbed()
         .setDescription(
-          "Wanna give you Ban Member permission, Fine! but Why?"
+          "You are still just a kid, don't use this command! :smirk:"
         )
         .setColor("#2C2F33");
       message.channel.send(nopermsembed);
