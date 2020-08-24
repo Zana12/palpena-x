5e933030486d6027598058ce08a200dd150644e0
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-24 20:42:39 +0200

    Update

--- commands/hug.js
+++ commands/hug.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Test Purposes',
 	execute(message, args) {
         const taggedPerson = message.mentions.members.first();
-        if(!args[0]) {
+        if(!args.lentgh) {
             message.channel.send(`ئەوەی ئەتەوێ باوەشی پیاکەی تاگیکە هەناسە!`);
         } else {
             message.channel.send(`!${message.author} **باوەشێکی گەرمی کرد بە** ${taggedPerson} <:hug:747519226243448904>`)
