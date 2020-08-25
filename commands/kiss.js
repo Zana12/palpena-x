1bfb412d6e6c1884d803b8f0cb7b75497eb22a86
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-25 17:24:57 +0200

    lets update

--- commands/kiss.js
+++ commands/kiss.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Kissing',
 	execute(message, args) {
         const taggedPerson = message.mentions.members.first();
-		if (!args.length) return message.channel.send(`ئەوەی ئەتەوێ باوەشی پیاکەی تاگیکە هەناسە!`);
+		if (!args.length) return message.channel.send(`ئەو کەسە تاگ بکە کە ئەتەوێ ماچی گووپی بکەیت`);
 		message.delete(message.author);
         message.channel.send(`${message.author} \n **ماچێکی گووپی ** \n <a:machygup:747801955212066866> ${taggedPerson} **ی کرد**`);
 	},
