2c4a697a47892d93e63bb8546675ac33f0db934f
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-25 17:37:51 +0200

    lets update

--- commands/maza.js
+++ commands/maza.js
@@ -1,10 +1,15 @@
+const Discord = require('discord.js');
 module.exports = {
 	name: 'mazakrdn',
 	description: 'Maza',
 	execute(message, args) {
         const taggedPerson = message.mentions.members.first();
-		if (!args.length) return message.channel.send(`ئەو کەسە تاگ بکە کە ئەتەوێ مەزەی بکەی`);
-		message.delete(message.author);
+        if (!args.length) return message.channel.send(`ئەو کەسە تاگ بکە کە ئەتەوێ مەزەی بکەی`);
+        if (!message.member.permissions.has("ADMINISTRATOR")) {
+            message.channel.send("**ببورە تۆ دەسەڵاتی ئەوەو نیە هیچ کەسێ مەزە بکەی** :smirk:");
+            return;
+        }
+        message.delete(message.author);
         message.channel.send(`${message.author} \n **هەستا بە مەزەکردنی** \n <:txa:746460815414132788> ${taggedPerson}`);
 	},
 };
