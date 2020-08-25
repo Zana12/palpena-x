b0efb6eb8be257a1d4cc7e65cc297c6882219fc9
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-25 17:25:34 +0200

    Create maza.js

---
+++ commands/maza.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'maza',
+	description: 'Maza',
+	execute(message, args) {
+        const taggedPerson = message.mentions.members.first();
+		if (!args.length) return message.channel.send(`ئەو کەسە تاگ بکە کە ئەتەوێ مەزەی بکەی`);
+		message.delete(message.author);
+        message.channel.send(`${message.author} \n **هەستا بە مەزەکردنی** \n <:txa:746460815414132788> ${taggedPerson}`);
+	},
+};
