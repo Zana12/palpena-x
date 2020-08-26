2f38eb9c9a657796a470c08993f54ae076c784fd
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-26 18:37:38 +0200

    Create event.js

---
+++ commands/event.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+	name: 'ping',
+	description: 'Test Purposes',
+	execute(message, args) {
+		let age = args[0]; // Remember arrays are 0-based!.
+        let sex = args[1];
+        let location = args[2];
+        message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+	},
+};
