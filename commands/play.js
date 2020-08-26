272e9967e63123393a9f0f20238b6cb59a771813
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-08-26 16:34:29 +0200

    lets update

--- commands/play.js
+++ commands/play.js
@@ -1,23 +1,26 @@
-const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
+const config = require('../config.json');
 
 module.exports = {
 	name: 'play',
 	description: 'Test Purposes',
 	async execute(message, args) {
         if (message.channel.type === 'dm') return;
+		if (!args.length) return message.channel.send(`:warning: **Usage:** \`${config.prefix}play <link>\``);
 
 		const voiceChannel = message.member.voice.channel;
 
 		if (!voiceChannel) {
 			return message.reply('please join a voice channel first!');
-		}
+        }
+        
+        if(args[0] === message.content) {
+            voiceChannel.join().then(connection => {
+                const stream = ytdl(args, { filter: 'audioonly' });
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish', () => voiceChannel.leave());
+            });
+        }
 
-		voiceChannel.join().then(connection => {
-			const stream = ytdl('https://youtu.be/QfqFNVBjZ_U', { filter: 'audioonly' });
-			const dispatcher = connection.play(stream);
-
-			dispatcher.on('finish', () => voiceChannel.leave());
-		});
 	},
 };
