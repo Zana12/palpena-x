c593a72b10a2033b1979df882a13ea9e23a54013
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-09-01 18:28:21 +0200

    lets update

--- index.js
+++ index.js
@@ -80,14 +80,13 @@ client.on('message', async message => {
 
 client.on('message', async message => {
 	if (channel.id === '667011220477444135') {
-		// You can ignore all bot messages like this
 		if (member.user.bot) return;
 		let count = 0;
 		if (Number(content) === count + 1) {
 		  count++;
 		  console.log(count++);
 		} else if (member.id !== client.user.id) {
-		  channel.send(`${member} messed up!`).catch(console.error);
+		  message.channel.send(`${member} messed up!`).catch(console.error);
 		  count = 0;
 		}
 	  }
