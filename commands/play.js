d9d19303c9fde61a47b5ec1c026e79f874df22af
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-09-07 16:17:29 +0200

    lets update

--- commands/play.js
+++ commands/play.js
@@ -6,13 +6,13 @@ module.exports = {
 	description: 'Test Purposes',
 	execute(message, args) {
 		let ytLink = args[0];
-
+		if(!ytLink.length) return message.channel.send(`> **ئێ کاکە جارێ لینکەکە دانێ دە با نەبێ بە ناخۆشیمان**`)
 		if (message.channel.type === 'dm') return;
 
 		const voiceChannel = message.member.voice.channel;
 
 		if (!voiceChannel) {
-			return message.reply('please join a voice channel first!');
+			return message.reply('> **ئەوە تۆ تەواوی؟ جارێ بڕۆ ڤۆیس ئینجا من بانگکە کورە**');
 		}
 
 		voiceChannel.join().then(connection => {
