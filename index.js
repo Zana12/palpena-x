b217339f5c38ac8911f4b0ec5a9935f7301b2ea5
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-09-13 14:26:18 +0200

    lets update

--- index.js
+++ index.js
@@ -74,17 +74,11 @@ client.on('guildMemberAdd', member => {
 	console.log(`${member.user.tag} Joined.`);
 
 	member.guild.fetchInvites().then(guildInvites => {
-		// This is the *existing* invites for the guild.
 		const ei = invites[member.guild.id];
-		// Update the cached invites for the guild.
 		invites[member.guild.id] = guildInvites;
-		// Look through the invites, find the one for which the uses went up.
 		const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-		// This is just to simplify the message being sent below (inviter doesn't have a tag property)
 		const inviter = client.users.cache.get(invite.inviter.id);
-		// Get the log channel (change to your liking)
 		const logChannel = member.guild.channels.cache.find(channel => channel.name === "member-invites");
-		// A real basic message with the information we need. 
 		logChannel.send(`<@${member.id}> joined using invite code \`${invite.code}\` from \`${inviter.tag}\`. Invite was used \`${invite.uses}\` times since its creation.`);
 	  });
 });
