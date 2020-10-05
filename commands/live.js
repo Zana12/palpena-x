1ea8b0cf92ea28428bb0b5a5c8d41cc0751dca90
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-10-05 13:19:51 +0200

    lets update

--- commands/live.js
+++ commands/live.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: 'live',
 	description: 'executing the server command',
 	async execute(message, args) {
-		let eventName = args[0]; // Remember arrays are 0-based!.
-		message.channel.send(`${eventName}`);
+        let Streamer = args[0]; // Remember arrays are 0-based!.
+        let live = args[1];
+		message.channel.send(`${eventName}, ${live}`);
 	},
 };
