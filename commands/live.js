b210e33d10b44a2928fb50c5f37732d1a50c8c1c
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-10-05 13:56:05 +0200

    lets update

--- commands/live.js
+++ commands/live.js
@@ -2,8 +2,22 @@ module.exports = {
 	name: 'live',
 	description: 'executing the server command',
 	async execute(message, args) {
-        let Streamer = args[0]; // Remember arrays are 0-based!.
-        let live = args[1];
-		message.channel.send(`${Streamer}, ${live}`);
+        let livelink = args[0];
+        const channel = member.guild.channels.cache.find(ch => ch.name === 'palpena-stream');
+        const embed = {
+            "title": "Palpena Team is Live Now!",
+            "description": `<:facebook:732173439183355904> We are Streaming now on Facebook make sure to show some Support to our Team! \n \n \n **LINK:** **[Click here](${livelink})**`,
+            "url": "https://discordapp.com",
+            "color": 4360181,
+            "thumbnail": {
+              "url": "https://res.cloudinary.com/zanakarzan/image/upload/v1599319328/tcfw3dc97522bb7lmylp.png"
+            },
+            "author": {
+              "name": "Palpena Team",
+              "icon_url": "https://palpena.com/assets/SQUAD.png"
+            }
+          };
+        message.delete(message.author);
+		channel.send(`<@&732904920306548806>,`,{embed});
 	},
 };
