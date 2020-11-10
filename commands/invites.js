f91727637522722a4f8bcaa7448bb3a3abe77fbd
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-11-10 15:13:54 +0100

    New One!

---
+++ commands/invites.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js'),
+arraySort = require('array-sort'),
+table = require('table');
+
+module.exports = {
+	name: 'invites',
+	description: 'Invite Table',
+	async execute(message, args) {
+        let invites = await message.guild.fetchInvites().catch(error => {
+            return message.channel.send("Sorry, I don't have the proper permission to view invites");
+            console.log(error);
+        });
+        invites = invites.array();
+        arraySort(invites, 'uses', {reverse: true});
+
+        let possibleInvites = [['User', 'Uses']];
+        invites.forEach(function(invite) {
+            possibleInvites.push([invite.inviter.username, invite,uses]);
+        });
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(0xCB5A5E)
+            .setAuthor('PalpenaTeam Server Invites', 'https://palpena.com/assets/SQUAD.png')
+            .addField('Leaderboard', `\`\`\`${table.table(possibleInvites)}\`\`\``);
+        
+        message.channel.send(embed);
+	},
+};
