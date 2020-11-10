0d2c20fa3a7b08826349c26a86c48f633b2a4337
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-11-10 16:39:19 +0100

    again buddy

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,9 @@
 module.exports = {
 	name: 'say',
 	description: 'executing the Say command',
-	async execute(message, args) {
-        let msg = args[0].slice(1).join(" ");
-        message.delete(message.author);
-		channel.send(msg);
+	execute(message, args) {
+        let text = args.join(" ");
+        message.delete(message.author)
+        message.channel.send(text);
 	},
 };
