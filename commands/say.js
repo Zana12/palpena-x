ace6420010e74489373848b269ca135e41fc7213
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-11-10 17:03:38 +0100

    again buddy

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,10 @@ module.exports = {
 	description: 'executing the Say command',
 	execute(message, args) {
         let text = args.join(" ");
+        if (!message.member.permissions.has("ADMINISTRATOR")) {
+            message.channel.send(`قەشمەری قۆمچە تۆ ناتوانی هیچ شتێکم پێ بڵێی`);
+            return;
+        }
         message.delete(message.author)
         message.channel.send(text);
 	},
