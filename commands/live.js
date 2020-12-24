52f0fc61e53bb4a6fb010f4caed8c9b8f8e80f0d
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2020-12-24 20:32:28 +0100

    commit

--- commands/live.js
+++ commands/live.js
@@ -2,10 +2,13 @@ module.exports = {
 	name: 'live',
 	description: 'executing the server command',
 	async execute(message, args) {
-        let livelink = args[0];
-        if (!args.length) return message.channel.send(`ئەو لینکە دانێ بەهەشتی، یاپراخ لە ئاگاداری دایناوە جوان سەیریکە <:nababa:762646412470386719>`);
+        let scType = args[0];
+        let livelink = args[1];
+        // if (!args.length) return message.channel.send(`ئەو لینکە دانێ بەهەشتی، یاپراخ لە ئاگاداری دایناوە جوان سەیریکە <:nababa:762646412470386719>`);
         const channel = message.guild.channels.cache.find(ch => ch.name === 'palpena-stream');
-        const embed = {
+
+        if(scType === "f") {
+          const embed = {
             "title": "Palpena Team is Live Now!",
             "description": `<:facebook:732173439183355904> We are Streaming now on Facebook make sure to show some Support to our Team! \n \n \n **LINK:** **[Click here](${livelink})**`,
             "url": "https://discordapp.com",
@@ -18,7 +21,24 @@ module.exports = {
               "icon_url": "https://palpena.com/assets/SQUAD.png"
             }
           };
+        } else if (scType === "y") {
+          const embed = {
+            "title": "Palpena Team is Live Now!",
+            "description": `<:YouTube:760546309282594856> We are Streaming now on YouTube make sure to show some Support to our Team! \n \n \n **LINK:** **[Click here](${livelink})**`,
+            "url": "https://discordapp.com",
+            "color": 4360181,
+            "thumbnail": {
+              "url": "https://res.cloudinary.com/zanakarzan/image/upload/v1599319328/tcfw3dc97522bb7lmylp.png"
+            },
+            "author": {
+              "name": "Palpena Team",
+              "icon_url": "https://palpena.com/assets/SQUAD.png"
+            }
+          };
+        }
+        
         message.delete(message.author);
-		channel.send(`<@&732904920306548806>,`,{embed});
+    channel.send({embed});
+    // `<@&732904920306548806>,`,
 	},
 };
