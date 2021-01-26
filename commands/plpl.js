ab534b1ed7f466f4fe17431498f269851832c355
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2021-01-26 22:10:45 +0100

    files

---
+++ commands/plpl.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+	name: 'plpl',
+	description: 'PLPL Stats',
+	async execute(message, args) {
+        const file = await fetch('https://palpena.com/api?leaderboard').then(response => response.json());
+        let sup = "";
+        let rank = 1;
+        file.forEach((pro) => {
+            sup += `\`#${rank++}\`  | **${pro[8]}** | \`PTS:\` **${pro[19]}**\n`;
+        });
+        const embed = new Discord.MessageEmbed()
+            embed.setColor('#6C5CE7')
+            embed.setTitle('**Palpena League Standing**')
+            embed.setThumbnail('https://palpena.com/dash/media/palpena_league_logo.png')
+            embed.setDescription(`${sup}\n <:palpena_league:803668112821846026> - [PLPL Overall Standing](https://palpena.com/palpena-league)`)
+            embed.setTimestamp()
+            embed.setImage('https://palpena.com/dash/media/Palpena_Legaue_Stand.png')
+            embed.setAuthor('Palpena Team', 'https://palpena.com/dash/media/Palpena_White.png', 'https://palpena.com/')
+            embed.setFooter(`Palpena Team | Requested by ${message.author.username}`, 'https://palpena.com/dash/media/Palpena_White.png');
+        message.channel.send(embed);
+    }
+};
---
+++ commands/profiles.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+	name: 'profile',
+	description: 'Profiles Fetch from the Web',
+	async execute(message, args) {
+        if (!args.length) {
+            return message.channel.send(`Please write the nickname correctly!\n > If the nickname contains spaces please don't forget to replace the space with \`_\`.`);
+        }
+        const profileargument = args[0].replace('_', '%20');
+        const profile = await fetch('https://palpena.com/api?pro='+profileargument).then(response => response.json());
+        const plp_name = profile[0][1];
+        const plp_banner  = profile[0][2];
+        const plp_profile_picture = profile[0][3];
+        const plp_badge_vip = profile[0][4];
+        const plp_badge_supporter = profile[0][5];
+        const plp_badge_staff = profile[0][6];
+        const plp_discord_role = profile[0][7];
+        const plp_nickname = profile[0][8];
+        const plp_about = profile[0][9];
+        const plp_location = profile[0][10];
+        const plp_profession = profile[0][11];
+        const plp_birthday = profile[0][12];
+        const plp_facebook = profile[0][13];
+        const plp_instagram = profile[0][14];
+        const plp_snapchat = profile[0][15];
+        const plp_tiktok = profile[0][16];
+        const plp_twitter = profile[0][17];
+        const plp_setting_birthday = profile[0][18];
+        const plp_plpl_point = profile[0][19];
+        
+        let plp_ph_birthday = "";
+        let plp_ph_about = "";
+        let plp_ph_vip = "";
+        let plp_ph_supporter = "";
+        let plp_ph_staff = "";
+        let plp_ph_role_emoji = "";
+
+        let plp_ph_facebook = "";
+        let plp_ph_instagram = "";
+        let plp_ph_tiktok = "";
+        let plp_ph_snapchat = "";
+        let plp_ph_twitter = "";
+        if(plp_discord_role === "Owner") { plp_ph_role_emoji += "<:Owner:800388581545213962>"; } else if(plp_discord_role === "Organizer") { plp_ph_role_emoji += "<:Organizer:800388582275153951>"; } else if(plp_discord_role === "Administrator") { plp_ph_role_emoji += "<:Administrator:800388582220365882>"; } else if(plp_discord_role === "Grand_Master") { plp_ph_role_emoji += "<:Grand_Master:800388581550063646>"; } else if(plp_discord_role === "Master") { plp_ph_role_emoji += "<:Master:800388581168119850>"; } else if(plp_discord_role === "Expert") { plp_ph_role_emoji += "<:Expert:800388582211715113>"; } else if(plp_discord_role === "Epic_Guard") { plp_ph_role_emoji += "<:Epic_Guard:800388582690914314>"; } else if(plp_discord_role === "Lady_Soldier") { plp_ph_role_emoji += "<:Lady_Soldier:800388582271352842>"; } else if(plp_discord_role === "Lady_Soldier_I") { plp_ph_role_emoji += "<:Lady_Soldier_I:800388582295863316>"; } else if(plp_discord_role === "Lady_Soldier_II") { plp_ph_role_emoji += "<:Lady_Soldier_II:800388582295732334>"; } else if(plp_discord_role === "Lady_Soldier_III") { plp_ph_role_emoji += "<:Lady_Soldier_III:800388582073827329>"; } else if(plp_discord_role === "Co_Owner") { plp_ph_role_emoji += "<:Co_Owner:800388581456740392>"; } else if(plp_discord_role === "Soldier") { plp_ph_role_emoji += "<:Soldier:800388582274498580>"; } else if(plp_discord_role === "Soldier_I") { plp_ph_role_emoji += "<:Soldier_I:800388582266634240>"; } else if(plp_discord_role === "Soldier_II") { plp_ph_role_emoji += "<:Soldier_II:800388582430474240>"; } else if(plp_discord_role === "Soldier_III") { plp_ph_role_emoji += "<:Soldier_III:800388582283804702>"; } else if(plp_discord_role === "Streamer") { plp_ph_role_emoji += "<:Streamer:800388582057574411>"; } else if(plp_discord_role === "Team_Manager") { plp_ph_role_emoji += "<:Team_Manager:800388582002786305>"; } else if(plp_discord_role === "Admin") { plp_ph_role_emoji += "<:Admin:800388581793333269>"; }
+        if(plp_badge_vip === "on") {plp_ph_vip = "<:vip:803612700100788264>";}if(plp_badge_supporter === "on") {plp_ph_supporter = "<:supporter:803612700071559169>";}if(plp_badge_staff === "on") {plp_ph_staff = "<:staff:803612700038004846>";}
+        if(plp_about !== "") {plp_ph_about += plp_about;}
+        if(plp_setting_birthday === "on") {plp_ph_birthday += plp_birthday;} else {plp_ph_birthday += "hidden";}
+
+        if(plp_facebook !== "") {plp_ph_facebook += `<:facebook:803717707332976641> - [Facebook](${plp_facebook})`;} 
+        if(plp_instagram !== "") {plp_ph_instagram += `<:instagram:803717773242138644> - [Instagram](${plp_instagram})`;}
+        if(plp_tiktok !== "") {plp_ph_tiktok += `<:tiktok:803717846139928597> - [TikTok](${plp_tiktok})`;}
+        if(plp_snapchat !== "") {plp_ph_snapchat += `<:snapchat:803629330303156244> - [Snapchat](${plp_snapchat})`;}
+        if(plp_twitter !== "") {plp_ph_twitter += `<:twitter:803629382408863754> - [Twitter](${plp_twitter})`;}
+        const embed = new Discord.MessageEmbed()
+            embed.setColor('#6C5CE7')
+            embed.setTitle(`**${plp_nickname}**`)
+            embed.setDescription(`${plp_ph_about}\n\n**Real Name:** ${plp_name} \n**Base Role:** ${plp_ph_role_emoji} ${plp_discord_role} \n**Location:** ${plp_location} \n**Profession:** ${plp_profession} \n**Badges:** ${plp_ph_vip} ${plp_ph_supporter} ${plp_ph_staff} \n**Birthday:** ${plp_ph_birthday}\n**PLPL Points:** ${plp_plpl_point}\n\n\n${plp_ph_facebook} ${plp_ph_instagram} ${plp_ph_tiktok} ${plp_ph_snapchat} ${plp_ph_twitter}`)
+            embed.setThumbnail(`https://palpena.com/dash/media/profiles/pfp/${plp_profile_picture}`)
+            embed.setImage(`https://palpena.com/dash/media/profiles/banners/${plp_banner}`)
+            embed.setTimestamp()
+            embed.setAuthor('Palpena Team', 'https://palpena.com/dash/media/Palpena_White.png', 'https://palpena.com/')
+            embed.setFooter(`Palpena Team | Requested by ${message.author.username}`, 'https://palpena.com/dash/media/Palpena_White.png');
+        message.reply(embed);
+    }
+};
