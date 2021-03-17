990628667cce23b4b86657aa8b16bdbad8527075
Zana K. Hassan <31017065+Zana12@users.noreply.github.com>
2021-03-17 21:37:10 +0100

    files

--- commands/plpl.js
+++ commands/plpl.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'plpl',
 	description: 'PLPL Stats',
 	async execute(message, args) {
-        const file = await fetch('https://palpena.com/api?leaderboard').then(response => response.json());
+        const file = await fetch('https://palpenateam.com/api?leaderboard').then(response => response.json());
         let sup = "";
         let rank = 1;
         file.forEach((pro) => {
@@ -13,12 +13,12 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             embed.setColor('#6C5CE7')
             embed.setTitle('**Palpena League Standing**')
-            embed.setThumbnail('https://palpena.com/dash/media/palpena_league_logo.png')
-            embed.setDescription(`${sup}\n <:palpena_league:803668112821846026> - [PLPL Overall Standing](https://palpena.com/palpena-league)`)
+            embed.setThumbnail('https://palpenateam.com/dash/media/palpena_league_logo.png')
+            embed.setDescription(`${sup}\n <:palpena_league:803668112821846026> - [PLPL Overall Standing](https://palpenateam.com/palpena-league)`)
             embed.setTimestamp()
-            embed.setImage('https://palpena.com/dash/media/Palpena_Legaue_Stand.png')
-            embed.setAuthor('Palpena Team', 'https://palpena.com/dash/media/Palpena_White.png', 'https://palpena.com/')
-            embed.setFooter(`Palpena Team | Requested by ${message.author.username}`, 'https://palpena.com/dash/media/Palpena_White.png');
+            embed.setImage('https://palpenateam.com/dash/media/Palpena_Legaue_Stand.png')
+            embed.setAuthor('Palpena Team', 'https://palpenateam.com/dash/media/Palpena_White.png', 'https://palpenateam.com/')
+            embed.setFooter(`Palpena Team | Requested by ${message.author.username}`, 'https://palpenateam.com/dash/media/Palpena_White.png');
         message.channel.send(embed);
     }
 };
